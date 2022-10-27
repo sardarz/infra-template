76f3bb845add6f567882e199ce958ca4bf7354b0
sardarz <muhkween@gmail.com>
2022-10-28 01:04:34 +0300

    add partial releaseTicket function

--- scripts/ticket.js
+++ scripts/ticket.js
@@ -1,25 +1,41 @@
 import fetch from "node-fetch";
-import exec from "@actions/exec"
-import github from "@actions/github"
+import exec from "@actions/exec";
+import github from "@actions/github";
 
-const {AUTH, ORG_ID} = process.env
+const { AUTH, ORG_ID } = process.env;
 
 const headers = {
   Authorization: `OAuth ${AUTH}`,
-  "X-Org-ID": ORG_ID
+  "X-Org-ID": ORG_ID,
+};
+
+async function releaseTicket() {
+  const ticketURL = `https://api.tracker.yandex.net/v2/issues/HOMEWORKSHRI-168`;
+  const author = github.context.actor;
+  const regex = /rc-\d+-\d+-\d+/
+  const ref = github.context.ref
+  const tag = ref.match(regex)[0];
+  const patchVersion = tag.split(".").pop();
+
+  let desc = getDescription(patchVersion)
+  console.log("desc", desc)
 }
 
-const ticketURL = `https://api.tracker.yandex.net/v2/issues/HOMEWORKSHRI-168`
-const author = github.context.author
-const tag = github.context.ref
-
-function releaseTicket() {
-  console.log("author: ", author)
-  console.log("tag: ", tag)
-  console.log(github.context)
-  console.log("-----------------")
-  console.log("exec", exec)
-  console.log("github", github)
+async function getDescription(patchVersion) {
+  `git log --pretty="%h %cn %B" rc-0.0.0...rc-0.0.1`;
+  let myOutput = "";
+  let myError = "";
+  const options = {};
+  options.listeners = {
+    stdout: (data) => {
+      myOutput += data.toString();
+    },
+    stderr: (data) => {
+      myError += data.toString();
+    },
+  };
+  let version = patchVersion === 0 ? "rc-0.0.0" : `rc-0.0.${patchVersion - 1}...rc-0.0.${patchVersion}`
+  return exec.exec("git log", [`--pretty="%h %cn %B" ${version}`], options)
 }
 
-releaseTicket()
+releaseTicket();
